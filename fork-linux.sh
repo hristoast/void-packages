283a861723a9b344ffc74e2cda2683981c5bf059
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2024-12-25 10:44:37 -0600

    New package: linux6.12-tkg-pds*

--- fork-linux.sh
+++ fork-linux.sh
@@ -5,44 +5,44 @@ version=${KERNVER-6.12}
 
 cd "$(realpath "$(dirname "${0}")")/srcpkgs"
 
-if ! [[ -d linux$version-tkg ]]; then
-    cp -ar linux"$version" linux"$version"-tkg
-    ln -sv linux"$version"-tkg linux"$version"-tkg-dbg
-    ln -sv linux"$version"-tkg linux"$version"-tkg-headers
+if ! [[ -d linux$version-tkg-pds ]]; then
+    cp -ar linux"$version" linux"$version"-tkg-pds
+    ln -sv linux"$version"-tkg-pds linux"$version"-tkg-pds-dbg
+    ln -sv linux"$version"-tkg-pds linux"$version"-tkg-pds-headers
 fi
 
-if ! [[ -d linux$version-tkg-alderlake ]]; then
-    cp -ar linux"$version" linux"$version"-tkg-alderlake
-    ln -sv linux"$version"-tkg-alderlake linux"$version"-tkg-alderlake-dbg
-    ln -sv linux"$version"-tkg-alderlake linux"$version"-tkg-alderlake-headers
+if ! [[ -d linux$version-tkg-pds-alderlake ]]; then
+    cp -ar linux"$version" linux"$version"-tkg-pds-alderlake
+    ln -sv linux"$version"-tkg-pds-alderlake linux"$version"-tkg-pds-alderlake-dbg
+    ln -sv linux"$version"-tkg-pds-alderlake linux"$version"-tkg-pds-alderlake-headers
 fi
 
-if ! [[ -d linux$version-tkg-zen ]]; then
-    cp -ar linux"$version" linux"$version"-tkg-zen
-    ln -sv linux"$version"-tkg-zen linux"$version"-tkg-zen-dbg
-    ln -sv linux"$version"-tkg-zen linux"$version"-tkg-zen-headers
+if ! [[ -d linux$version-tkg-pds-zen ]]; then
+    cp -ar linux"$version" linux"$version"-tkg-pds-zen
+    ln -sv linux"$version"-tkg-pds-zen linux"$version"-tkg-pds-zen-dbg
+    ln -sv linux"$version"-tkg-pds-zen linux"$version"-tkg-pds-zen-headers
 fi
 
 cd ..
 
-sed -i "s|^linux$version|linux$version-tkg-alderlake|;s|'linux$version'|'linux$version-tkg-alderlake'|" srcpkgs/linux"$version"-tkg-alderlake/template
-sed -i "s|^linux$version|linux$version-tkg-zen|;s|'linux$version'|'linux$version-tkg-zen'|" srcpkgs/linux"$version"-tkg-zen/template
-sed -i "s|^linux$version|linux$version-tkg|;s|'linux$version'|'linux$version-tkg'|" srcpkgs/linux"$version"-tkg/template
-sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg-alderlake_\${revision}\"|" srcpkgs/linux"$version"-tkg-alderlake/template
-sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg-zen_\${revision}\"|" srcpkgs/linux"$version"-tkg-zen/template
-sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg_\${revision}\"|" srcpkgs/linux"$version"-tkg/template
-sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg-alderlake|" srcpkgs/linux"$version"-tkg-alderlake/template
-sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg-zen|" srcpkgs/linux"$version"-tkg-zen/template
-sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg|" srcpkgs/linux"$version"-tkg/template
-sed -i "s|maintainer=\".* <.*@.*>|maintainer=\"Hristos <me@hristos.co>|" srcpkgs/linux"$version"-tkg-alderlake/template srcpkgs/linux"$version"-tkg-zen/template srcpkgs/linux"$version"-tkg/template
-sed -i "s|series)\"|series with TKG patches)\"|" srcpkgs/linux"$version"-tkg-alderlake/template srcpkgs/linux"$version"-tkg-zen/template srcpkgs/linux"$version"-tkg/template
-sed -i "s|archs=\"x86_64\* i686\* aarch64\*\"|archs=\"x86_64\*\"|" srcpkgs/linux"$version"-tkg-alderlake/template srcpkgs/linux"$version"-tkg-zen/template
-sed -i "s|\tsed -i -e \"s\|\^\\\(CONFIG|\t#sed -i -e \"s\|\^\\\(CONFIG|" srcpkgs/linux"$version"-tkg-alderlake/template srcpkgs/linux"$version"-tkg-zen/template srcpkgs/linux"$version"-tkg/template
-sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg-alderlake_1\"|" srcpkgs/linux"$version"-tkg-alderlake/files/x86_64-dotconfig
-sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg-zen_1\"|" srcpkgs/linux"$version"-tkg-zen/files/x86_64-dotconfig
-sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg_1\"|" srcpkgs/linux"$version"-tkg/files/*
+sed -i "s|^linux$version|linux$version-tkg-pds-alderlake|;s|'linux$version'|'linux$version-tkg-pds-alderlake'|" srcpkgs/linux"$version"-tkg-pds-alderlake/template
+sed -i "s|^linux$version|linux$version-tkg-pds-zen|;s|'linux$version'|'linux$version-tkg-pds-zen'|" srcpkgs/linux"$version"-tkg-pds-zen/template
+sed -i "s|^linux$version|linux$version-tkg-pds|;s|'linux$version'|'linux$version-tkg-pds'|" srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg-pds-alderlake_\${revision}\"|" srcpkgs/linux"$version"-tkg-pds-alderlake/template
+sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg-pds-zen_\${revision}\"|" srcpkgs/linux"$version"-tkg-pds-zen/template
+sed -i "s|_kernver=\"\${version}_\${revision}\"|_kernver=\"\${version}-tkg-pds_\${revision}\"|" srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg-pds-alderlake|" srcpkgs/linux"$version"-tkg-pds-alderlake/template
+sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg-pds-zen|" srcpkgs/linux"$version"-tkg-pds-zen/template
+sed -i "s|pkgname=linux$version|pkgname=linux$version-tkg-pds|" srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|maintainer=\".* <.*@.*>|maintainer=\"Hristos <me@hristos.co>|" srcpkgs/linux"$version"-tkg-pds-alderlake/template srcpkgs/linux"$version"-tkg-pds-zen/template srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|series)\"|series with TKG patches)\"|" srcpkgs/linux"$version"-tkg-pds-alderlake/template srcpkgs/linux"$version"-tkg-pds-zen/template srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|archs=\"x86_64\* i686\* aarch64\*\"|archs=\"x86_64\*\"|" srcpkgs/linux"$version"-tkg-pds-alderlake/template srcpkgs/linux"$version"-tkg-pds-zen/template
+sed -i "s|\tsed -i -e \"s\|\^\\\(CONFIG|\t#sed -i -e \"s\|\^\\\(CONFIG|" srcpkgs/linux"$version"-tkg-pds-alderlake/template srcpkgs/linux"$version"-tkg-pds-zen/template srcpkgs/linux"$version"-tkg-pds/template
+sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg-pds-alderlake_1\"|" srcpkgs/linux"$version"-tkg-pds-alderlake/files/x86_64-dotconfig
+sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg-pds-zen_1\"|" srcpkgs/linux"$version"-tkg-pds-zen/files/x86_64-dotconfig
+sed -i "s|CONFIG_LOCALVERSION=\"_1\"|CONFIG_LOCALVERSION=\"-tkg-pds_1\"|" srcpkgs/linux"$version"-tkg-pds/files/*
 
-rm -f srcpkgs/linux"$version"-tkg-alderlake/files/arm64-dotconfig \
-   srcpkgs/linux"$version"-tkg-alderlake/files/i386-dotconfig \
-   srcpkgs/linux"$version"-tkg-zen/files/arm64-dotconfig \
-   srcpkgs/linux"$version"-tkg-zen/files/i386-dotconfig
+rm -f srcpkgs/linux"$version"-tkg-pds-alderlake/files/arm64-dotconfig \
+   srcpkgs/linux"$version"-tkg-pds-alderlake/files/i386-dotconfig \
+   srcpkgs/linux"$version"-tkg-pds-zen/files/arm64-dotconfig \
+   srcpkgs/linux"$version"-tkg-pds-zen/files/i386-dotconfig
